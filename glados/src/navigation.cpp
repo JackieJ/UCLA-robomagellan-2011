987eacdc813812b4c97e3eb0239ffbbc6f7dd27c
Garrett Brown <garbearucla@gmail.com>
2012-04-15 07:34:28 -0700

    Testing out tf functionality

--- glados/src/navigation.cpp
+++ glados/src/navigation.cpp
@@ -22,6 +22,7 @@
 
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
+#include <tf/transform_listener.h>
 
 // Heads towards LS 5926 (target coords unused for now)
 double target_lat = 34.0673;
@@ -38,12 +39,22 @@ int main(int argc, char **argv)
 
 	// create new geometry_msgs::Twist that holds linear and angular velocities
 	geometry_msgs::Twist command;
-	command.linear.x = 0.1; // m/s
+	command.linear.x = 0; // m/s
 	command.linear.y = 0;
 	command.linear.z = 0;
 	command.angular.x = 0; // rad/s
 	command.angular.y = 0;
 	command.angular.z = 0;
+
+	tf::TransformListener listener;
+
+	// wait for tf to begin receiving this information about this transform from the odometry node
+	listener.waitForTransform("base_footprint", "odom_combined", ros::Time(0), ros::Duration(1, 0));
+
+	// record the starting transform from the odometry to the base frame
+	tf::StampedTransform start_transform;
+	tf::StampedTransform current_transform;
+	listener.lookupTransform("base_footprint", "odom_combined", ros::Time(0), start_transform);
 
 	// publish twist_msg until node is killed
 	ros::Rate loop_rate(10); // Hz
@@ -52,6 +63,21 @@ int main(int argc, char **argv)
 		vel_pub.publish(command);
 		ros::spinOnce();
 		loop_rate.sleep();
+
+		try
+		{
+			listener.lookupTransform("base_footprint", "odom_combined", ros::Time(0), current_transform));
+		}
+		catch (const tf::TransformException &ex)
+		{
+			ROS_ERROR("%s", ex.what());
+			break;
+		}
+
+		// See how far we've traveled
+		tf::Transform relative_transform = start_transform.inverse() * current_transform;
+		double dist_moved = relative_transform.getOrigin().length();
+
 	}
 
 	// Grind the robot to a halt
