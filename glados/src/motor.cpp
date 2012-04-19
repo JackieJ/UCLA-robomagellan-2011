167950eea6484a3475bafc3179a80bee612c4e2f
glados <glados@robot.(none)>
2012-04-18 18:23:14 -0700

    Updated Jackie's work on the motor node

--- glados/src/motor.cpp
+++ glados/src/motor.cpp
@@ -8,89 +8,100 @@
 #include <time.h>
 #include "std_msgs/Float64.h"
 #include "glados/odometry.h"
+//#include <math.h>
+
+#define CPR_ENCODER_VAL 10000
 
 using namespace std;
 
+const double PI = std::atan(1.0)*4;
+
 class gladosMotor{
-		
-		unsigned int refreshRate;		
-		AX3500  ax3500;
-		ros::Publisher odometry_pub;
-		
-	public:
-		
-		unsigned int ticks_per_sec;		
-		void setMotorSpeed(const geometry_msgs::Twist::ConstPtr& msg);
-		void refresh();
-		void initMotorController();
-		void Close();
-		gladosMotor(int&, char**);
+  
+  unsigned int refreshRate;		
+  AX3500  ax3500;
+  ros::Publisher odometry_pub;
+  float wheelbase;
+  float wheelDiameter;
+  //float PI;
+public:
+  
+  unsigned int ticks_per_sec;		
+  void setMotorSpeed(const geometry_msgs::Twist::ConstPtr& msg);
+  void refresh();
+  void initMotorController();
+  void Close();
+  gladosMotor(int&, char**);
 };
 
 gladosMotor::gladosMotor(int& argc, char** argv){
-	
-	ros::init(argc,argv,"motor_node");
-	ros::NodeHandle n;
-	
-	odometry_pub = n.advertise<glados::odometry>("/odometry", 1000);
-//	ros::Subscriber sub = n.subscribe("/cmd_vel", 1000, &gladosMotor::setMotorSpeed, &motorNodeH);
-//	ros::Subscriber sub = n.subscribe("/cmd_vel", 1000, &gladosMotor::setMotorSpeed, this);
+  
+  ros::init(argc,argv,"motor_node");
+  ros::NodeHandle n;
+  
+  odometry_pub = n.advertise<glados::odometry>("/odometry", 1000);
+  
+  wheelbase = 1.2;//m
+  wheelDiameter = .35;//m
+  //PI = 3.1415;
+  //ros::Subscriber sub = n.subscribe("/cmd_vel", 1000, &gladosMotor::setMotorSpeed, &motorNodeH);
+  //ros::Subscriber sub = n.subscribe("/cmd_vel", 1000, &gladosMotor::setMotorSpeed, this);
 }
 
 void gladosMotor::setMotorSpeed(const geometry_msgs::Twist::ConstPtr& msg)
 {
-    //typecasting
-    char linear  = (char)(((msg->linear).x)*100);
-    char angular = (char)(((msg->angular).x)*100);
-
-    //setting speed
-    ax3500.SetSpeed(AX3500::CHANNEL_LINEAR, -angular);
-    ax3500.SetSpeed(AX3500::CHANNEL_STEERING, -linear);
+  //typecasting
+  char linear  = (char)(((msg->linear).x)*100);
+  char angular = (char)(((msg->angular).x)*100);
+  
+  //setting speed
+  ax3500.SetSpeed(AX3500::CHANNEL_LINEAR, -angular);
+  ax3500.SetSpeed(AX3500::CHANNEL_STEERING, -linear);
 }
 
 void gladosMotor::refresh()
 {
-//	cout<<"starting refresh"<<endl;
-    int l_ticks, r_ticks;
-    ax3500.ReadEncoder(AX3500::ENCODER_1, AX3500::ABSOLUTE, l_ticks);
-    ax3500.ReadEncoder(AX3500::ENCODER_2, AX3500::ABSOLUTE, r_ticks);
-
-    
-    
-    glados::odometry msg;
-    msg.left = (l_ticks / 10000. ) * .35 * 3.1415; // This is God speaking. DONT USE NUMBERS - #DEFINE YOUR VARIABLES!  // this is reality speaking, build something, don't talk about it.
-    msg.right = (r_ticks / 10000. ) * .35 * 3.1415;
-    msg.offset = ((l_ticks / 10000. ) * .35 * 3.1415) - ((r_ticks / 10000. ) * .35 * 3.1415);
-    //   	cout<<"l encoder"<<l_msg.data<<endl;
-    //    ROS_INFO("wheel position %f,%f", l_msg.data,r_msg.data);
-    odometry_pub.publish(msg);
+  //cout<<"starting refresh"<<endl;
+  int l_ticks, r_ticks;
+  ax3500.ReadEncoder(AX3500::ENCODER_1, AX3500::ABSOLUTE, l_ticks);
+  ax3500.ReadEncoder(AX3500::ENCODER_2, AX3500::ABSOLUTE, r_ticks);
+  
+  glados::odometry msg;
+  msg.left = (float)(wheelDiameter * PI * l_ticks / CPR_ENCODER_VAL);
+  msg.right = (float)(wheelDiameter * PI * r_ticks / CPR_ENCODER_VAL);
+  msg.heading = (float)((((msg.left - msg.right) / wheelbase) * PI) * 360);
+  
+  //cout<<"l encoder"<<l_msg.data<<endl;
+  //ROS_INFO("wheel position %f,%f", l_msg.data,r_msg.data);
+  odometry_pub.publish(msg);
 }
 
 void gladosMotor::initMotorController()
 {
-    bool isOpen = false;
-    string USB_SERIAL_PORTS[4] = {"/dev/ttyUSB0","/dev/ttyUSB1","/dev/ttyUSB2","/dev/ttyUSB3"}; 
-	
-    //port connection sanity check
-    int serial_port_counter = 0;
-    while (!isOpen) {
-
-      cout<<"connecting to serial port..."<<endl;
-      
-      isOpen = ax3500.Open(USB_SERIAL_PORTS[serial_port_counter], true);
-      serial_port_counter++;
-      
-      if (serial_port_counter == 5) serial_port_counter = 0;
-          cout<<"looping again"<<endl;
-    }
+  bool isOpen = false;
+  //string USB_SERIAL_PORTS[4] = {"/dev/ttyUSB0","/dev/ttyUSB1","/dev/ttyUSB2","/dev/ttyUSB3"}; 
+  string USB_SERIAL_PORTS[1] = {"/dev/ttyUSB0"}; 
+  
+  //port connection sanity check
+  int serial_port_counter = 0;
+  while (!isOpen) {
     
-    cout << "Resetting encoders";
-    ax3500.ResetEncoder(AX3500::ENCODER_BOTH);
+    cout<<"connecting to serial port..."<<endl;
+    
+    isOpen = ax3500.Open(USB_SERIAL_PORTS[serial_port_counter], true);
+    serial_port_counter++;
+    
+    if (serial_port_counter == 5) serial_port_counter = 0;
+    cout<<"looping again"<<endl;
+  }
+  
+  cout << "Resetting encoders";
+  ax3500.ResetEncoder(AX3500::ENCODER_BOTH);
     
 }
 void gladosMotor::Close()
 {
-	ax3500.Close();
+  ax3500.Close();
 }
 
 int main(int argc, char **argv)
@@ -108,16 +119,15 @@ int main(int argc, char **argv)
 
   cout << "pre while";
 //  while (ros::ok())
-  while (true)
-	{
-//		cout << "start while";
-		motorNodeH.refresh();
-	    //    
-	    //... do some work ...
-		ros::spinOnce();
-//		cout << "spun once";
-	    r.sleep();
-	}
+  while (true) {
+    //		cout << "start while";
+    motorNodeH.refresh();
+    //    
+    //... do some work ...
+    ros::spinOnce();
+    //		cout << "spun once";
+    r.sleep();
+  }
   motorNodeH.Close();
   return 0;
 }
