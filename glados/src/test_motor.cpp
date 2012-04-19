cd0f53a9d37866e8969594ffa93da4331a2871fd
Garrett Brown <garbearucla@gmail.com>
2012-04-19 05:28:37 -0700

    Added motor tests, from 0.01m/s to 0.3m/s (this is using the faulty gain, 220; the gain should really be 22.0)

--- glados/src/test_motor.cpp
+++ glados/src/test_motor.cpp
@@ -17,7 +17,6 @@ public:
 	MotorTest();
 	~MotorTest() { }
 
-	void initTest(double speed);
 	void doTest(double speed);
 	void receiveSpeed(const glados::wheelspeed::ConstPtr &speed);
 
@@ -51,8 +50,6 @@ MotorTest::MotorTest()
 
 void MotorTest::receiveSpeed(const glados::wheelspeed::ConstPtr &speed)
 {
-	cout << "Here" << endl;
-	cout << "Received speed: " << speed->avg << endl;
 	speed_results = speed;
 }
 
@@ -60,28 +57,6 @@ glados::wheelspeed::ConstPtr MotorTest::getSpeed()
 {
 	return speed_results;
 }
-
-void MotorTest::initTest(double speed)
-{
-	if (ros::ok())
-	{
-		cout << "Initializing with a speed of " << speed << endl;
-		// Do the test here
-		geometry_msgs::Twist command;
-		command.linear.x = speed; // m/s
-		command.linear.y = 0;
-		command.linear.z = 0;
-		command.angular.x = 0; // rad/s
-		command.angular.y = 0;
-		command.angular.z = 0;
-		vel_pub.publish(command);
-		ros::spinOnce();
-		ros::Duration(5).sleep();
-		// Purge the callback queue
-		ros::spinOnce();
-	}
-}
-
 
 void MotorTest::doTest(double speed)
 {
@@ -99,7 +74,7 @@ void MotorTest::doTest(double speed)
 		vel_pub.publish(command);
 		ros::spinOnce(); // Is this necessary to publish our message?
 		// Give time for transients to settle
-		ros::Duration(1).sleep();
+		ros::Duration(5).sleep();
 		// Give enough time to fill the 10s buffer
 		ros::Duration(2).sleep();
 		ros::spinOnce(); // Purge the callback queue
@@ -121,9 +96,36 @@ int main(int argc, char **argv)
 
 	MotorTest test;
 
-	//test.initTest(0.01);
-	//test.doTest(0.01);
+	test.doTest(0.01);
+	test.doTest(0.02);
+	test.doTest(0.03);
+	test.doTest(0.04);
+	test.doTest(0.05);
+	test.doTest(0.06);
+	test.doTest(0.07);
+	test.doTest(0.08);
+	test.doTest(0.09);
+	test.doTest(0.10);
+	test.doTest(0.11);
+	test.doTest(0.12);
+	test.doTest(0.13);
+	test.doTest(0.14);
+	test.doTest(0.15);
+	test.doTest(0.16);
+	test.doTest(0.17);
+	test.doTest(0.18);
+	test.doTest(0.19);
+	test.doTest(0.20);
+	test.doTest(0.22);
+	test.doTest(0.24);
+	test.doTest(0.26);
+	test.doTest(0.28);
+	test.doTest(0.30);
+	test.doTest(0.20);
+	test.doTest(0.10);
+	test.doTest(0.05);
+	test.doTest(0.01);
+	test.doTest(0.0);
 
-	ros::spin();
 	return 0;
 }
